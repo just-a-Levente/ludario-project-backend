22b850d8eebfc50e5296f27cad64b772fb7dfd68
Kovács Levente <le.kovacs789@gmail.com>
2026-05-06 00:14:08 +0300

    created mongodb context

--- db.py
+++ db.py
@@ -1,8 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model.tables import Base
+from motor.motor_asyncio import AsyncIOMotorClient
+
+# ---------------------
+# FOR BOARDGAMES AND USER INFO
+# ---------------------
 
 DATABASE_URL = "postgresql+psycopg2://postgres:Leti_2004@localhost:5432/ludario"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
+
+# ---------------------
+# FOR CHAT MESSAGES
+# ---------------------
+
+MONGO_URL = "mongodb://localhost:27017"
+MONGO_DB_NAME = "ludario_chat"
+
+mongo_client = AsyncIOMotorClient(MONGO_URL)
+mongo_db = mongo_client[MONGO_DB_NAME]
+messages_collection = mongo_db["messages"]
