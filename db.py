cccb5293dee93695f70b82434fac788a71cc8421
Kovács Levente <le.kovacs789@gmail.com>
2026-06-02 01:10:02 +0300

    switching out the database links to external deployed ones

--- db.py
+++ db.py
@@ -1,13 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model.tables import Base
-from motor.motor_asyncio import AsyncIOMotorClient
+from pymongo import AsyncMongoClient
+from pymongo.server_api import ServerApi
 
 # ---------------------
 # FOR BOARDGAMES AND USER INFO
 # ---------------------
 
-DATABASE_URL = "postgresql+psycopg2://postgres:Leti_2004@localhost:5432/ludario"
+DATABASE_URL = "postgresql+psycopg2://admin:oPsox7FVja3txd14PLdeDwsJEXlPQGRn@dpg-d8evp3pkh4rs73enunpg-a.frankfurt-postgres.render.com/ludario_0y4l"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
 Base.metadata.create_all(engine)
@@ -16,9 +17,9 @@ Base.metadata.create_all(engine)
 # FOR CHAT MESSAGES
 # ---------------------
 
-MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = "mongodb+srv://lekovacs789_db_user:4gMA0PcncvOBDq7X@ludario-chat.rffissh.mongodb.net/?appName=ludario-chat"
 MONGO_DB_NAME = "ludario_chat"
 
-mongo_client = AsyncIOMotorClient(MONGO_URL)
+mongo_client = AsyncMongoClient(MONGO_URL, server_api=ServerApi('1'))
 mongo_db = mongo_client[MONGO_DB_NAME]
 messages_collection = mongo_db["messages"]
